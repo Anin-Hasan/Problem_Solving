c08724fe952a5209c142925c320077ab47b2daab
Anin-Hasan <200921@ku.ac.bd>
2024-12-28 11:34:46 +0600

    commonPrefix

---
+++ commonPrefix.js
@@ -0,0 +1,17 @@
+var longestCommonPrefix = function (strs) {
+  let commonPref = "";
+  for (let i = 0; i < strs[0].length; i++) {
+    let char = strs[0][i];
+    for (let j = 1; j < strs.length; j++) {
+      if (i >= strs[j].length || strs[j][i] !== char) {
+        return commonPref;
+      }
+    }
+    commonPref += char;
+  }
+  return commonPref;
+};
+
+strs = ["flower", "flow", "flight"];
+
+console.log(longestCommonPrefix(strs));
