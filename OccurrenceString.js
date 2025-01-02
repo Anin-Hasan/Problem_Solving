4ab100808047a16f62fd17a3326aa698f1bb2761
Anin-Hasan <200921@ku.ac.bd>
2025-01-02 19:09:36 +0600

    OccurenceString

---
+++ OccurrenceString.js
@@ -0,0 +1,8 @@
+var strStr = function (haystack, needle) {
+  let test = haystack.indexOf(needle);
+  return test;
+};
+
+const haystack = "hello";
+const needle = "asd";
+console.log(strStr(haystack, needle));
